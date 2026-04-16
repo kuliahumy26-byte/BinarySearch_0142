49cfdf78e5a3decae86913cfd0202d987cec3e0a
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-04-16 10:27:49 +0700

    menambahkan else didalam void

--- BinarySearch_0142.cpp
+++ BinarySearch_0142.cpp
@@ -12,6 +12,13 @@ void input()
    cout << "Masukkan banyaknya elemen pada array (maksimal 10) =";
     cin >> npanjang;
 
-    
+    if (npanjang <= 10 )
+    {
+        break;
+    } 
+    else
+    {
+        cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+    }
  }   
 }
