e746c77681e51e1a3c6652219f06487d89e54199
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-04-16 11:06:27 +0700

    menambahkan do if element high low

--- BinarySearch_0142.cpp
+++ BinarySearch_0142.cpp
@@ -74,7 +74,33 @@ void binarySeach()
     char ulang;
     do
     {
-        cout << "\n==========================================\n";
-        
+        cout << "\n==============================================\n";
+        cout << "             Pencarian Binary Search            \n";
+        cout << "================================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari : ";
+        cin >> x;
+
+        int low = 0;
+        int high = npanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+
+            if (element [mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid] )
+            {
+                low = mid + 1;
+            }
+        }
     }
 }
